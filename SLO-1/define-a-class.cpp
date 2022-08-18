6ed6736607cebb8a8072c72a1080fea9957b86b1
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-18 11:17:21 +0530

    Add project files.

---
+++ SLO-1/define-a-class.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
+
+int main() {
+	
+
+	return 0;
+}
---
+++ SLO-1/io-Operations.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+using namespace std;
+
+
+void make_func() {
+
+	class func
+	{
+	public:
+		func();
+		~func();
+
+	private:
+
+	};
+
+	
+
+};
+
+
+int main() {
+
+	const char* string1 = "test 1";
+
+	make_func();
+	
+
+	return 0;
+}
+
+
