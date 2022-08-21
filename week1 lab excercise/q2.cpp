b100a990d4658ff68d08d9d3fb881d96e83a068e
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-21 18:51:39 +0530

    .

--- week1 lab excercise/q2.cpp
+++ week1 lab excercise/q2.cpp
@@ -1,11 +1,20 @@
 // Week 1 lab Exercise
 // divisiblilty check
 
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
 int main() {
 
-	int num = 1000;
+	int num;
 	
-	
+	for (num = 1000; num <= 2000; num++) {
+		if (num % 5 == 0 && num % 7 == 0) {
+			cout << num << endl;
+		}
+	}
 
 	return 0;
 }
