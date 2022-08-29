35c423ec203e454ed75a1aac66fa09a3617d6802
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-30 01:12:18 +0530

    Update question.cpp

--- lab excercise week 3 and 4/question.cpp
+++ lab excercise week 3 and 4/question.cpp
@@ -1,36 +1,107 @@
-/* Extract all integers from string */
-#include <iostream>
-#include <sstream>
+#include<iostream>
+#include<string>
+
 using namespace std;
 
-void extractIntegerWords(string str)
-{
-	stringstream ss;
+class Date {
+private:
+	string str_month;
+	string str_date;
 
-	/* Storing the whole string into string stream */
-	ss << str;
-
-	/* Running loop till the end of the stream */
-	string temp;
-	int found;
-	while (!ss.eof()) {
-
-		/* extracting word by word from stream */
-		ss >> temp;
-
-		/* Checking the given word is integer or not */
-		if (stringstream(temp) >> found)
-			cout << found << " ";
-
-		/* To save from space at the end of string */
-		temp = "";
+public:
+	int date;
+	int month;
+	int year;
+	
+	Date(int x, int y, int z) {
+		date = x;
+		month = y;
+		year = z;
 	}
-}
+	
+	void convert_month() {
 
-// Driver code
-int main()
-{
-	string str = "1: 2 3 4 prakhar";
-	extractIntegerWords(str);
+		switch (month)
+		{
+		case 1:
+			str_month = "January";
+			break;
+		case 2:
+			str_month = "February";
+			break;
+		case 3:
+			str_month = "March";
+			break;
+		case 4:
+			str_month = "April";
+			break;
+		case 5:
+			str_month = "May";
+			break;
+		case 6:
+			str_month = "June";
+			break;
+		case 7:
+			str_month = "July";
+			break;
+		case 8:
+			str_month = "August";
+			break;
+		case 9:
+			str_month = "September";
+			break;
+		case 10:
+			str_month = "October";
+			break;
+		case 11:
+			str_month = "November";
+			break;
+		case 12:
+			str_month = "December";
+			break;
+		default:
+			break;
+		}
+	}
+	
+	void convert_date() {
+		switch (date%10)
+		{
+		case 1:
+			str_date = to_string(date) + "st ";
+			break;
+		case 2:
+			str_date = to_string(date) + "nd ";
+			break;
+		case 3:
+			str_date = to_string(date) + "rd ";
+			break;
+		default:
+			str_date = to_string(date) + "th ";
+			break;
+		}
+	}
+
+	void convert_format() {
+		this->convert_date();
+		this->convert_month();
+		
+		cout << str_date + str_month << " " << year << endl;
+	}
+	
+	
+};
+
+
+int main() {
+
+	int date, month, year;
+	
+	cin >> date >> month >> year;
+
+	Date test(date, month, year);
+	
+	test.convert_format();
+	
 	return 0;
 }
