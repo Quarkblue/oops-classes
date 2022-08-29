8dae54d7178ffb4f98929faed72c688155236449
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-08-30 00:01:34 +0530

    Update question.cpp

--- lab excercise week 3 and 4/question.cpp
+++ lab excercise week 3 and 4/question.cpp
@@ -1,29 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-// swap numbers in cyclic order
-void swap(int *a, int *b, int *c)
-{
-	int temp = *a;
-	*a = *b;
-	*b = *c;
-	*c = temp;
-}
-
 int main() {
 
-	int a, b, c;
-	
-	cin >> a >> b >> c;
-	
-	cout << "Values as (A), (B), (C): " << a << b << c << endl;
-	
-	swap(&a, &b, &c);
-	
-	cout << "Values after cyclic swap as (A), (B), (C): " << a << b << c << endl;
+
 
 	return 0;
 }
-
 
