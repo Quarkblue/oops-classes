acf20969f95085aeb1bb409c6ed2fed0a3a7a270
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-16 11:26:09 +0530

    .

---
+++ week4 lab excercises/question.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+class Date {
+
+	string day;
+	int month, year;
+	
+	Date(string d, int m, int y) {
+		day = d;
+		month = m;
+		year = y;
+		
+	}
+	
+	void changeformat() {
+		
+	}
+
+	
+};
+
+
+int main() {
+	
+	return 0;
+}
