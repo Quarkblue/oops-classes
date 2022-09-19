9d858c76c0dbc7c91bc9448ad1fd4829e9a03e5f
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-20 03:35:29 +0530

    Update question.cpp

--- week4 lab excercises/question.cpp
+++ week4 lab excercises/question.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-class Average {
+class Room {
+	
+	int length, breadth, height;
+	
 public:
-	static float calcAverage(int a, int b, int c)
-	{
-			float average = (a + b + c) / 3;
-			return average;
+	
+	Room(int l, int b, int h) {
+		length = l;
+		breadth = b;
+		height = h;
+	}
+
+	int area() {
+		
 	}
 	
 };
 
 int main() {
 
-	int x, y, z;
-	
-	cout << "Enter three numbers: ";
-	
-	cin >> x >> y >> z;
-	
-	cout << "Average of the numbers: " << Average::calcAverage(x, y, z) << endl;
-	
 	return 0;
 }
