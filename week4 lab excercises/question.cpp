d238111513c9e38b1e7d24da8114035ea84886e5
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-20 10:01:05 +0530

    Update question.cpp

--- week4 lab excercises/question.cpp
+++ week4 lab excercises/question.cpp
@@ -15,12 +15,23 @@ public:
 	}
 
 	int area() {
-		
+		int area = (2*(length * breadth)) + (2*(length * height)) + (2*(breadth * height));
+		return area;
+	}
+	
+	int volume() {
+		int volume = length * breadth * height;
+		return volume;
 	}
 	
 };
 
 int main() {
 
+	Room r1(3, 10, 4);
+	
+	cout << "Area of room: " << r1.area() << endl;
+	cout << "Volume of room: " << r1.volume() << endl;
+	
 	return 0;
 }
