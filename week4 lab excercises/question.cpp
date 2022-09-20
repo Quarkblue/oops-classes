2111fc4a72456cdadd9adfb190aa256eabc05068
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-20 10:17:00 +0530

    Update question.cpp

--- week4 lab excercises/question.cpp
+++ week4 lab excercises/question.cpp
@@ -1,17 +1,73 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int main()
-{
-    int i, ctr, cub;
 
-    cout << "\n\n Display the cube of the numbers upto a given integer:\n";
-    cout << "----------------------------------------------------------\n";
-    cout << "Input the number of terms : ";
-    cin >> ctr;
-    for (i = 1; i <= ctr; i++)
+
+class cube
+
+{
+
+public:
+
+    double side;
+
+    double volume()
+
     {
-        cub = i * i * i;
-        cout << "Number is : " << i << " and the cube of " << i << " is: " << cub << endl;
+
+        return(side * side * side);
+
     }
+
+    cube(double side1)
+
+    {
+
+        cout << "\nA constructor is called" << endl;
+
+        side = side1;
+
+    }
+
+    cube()
+
+    {
+
+        cout << "\nA default constructor is called " << endl;
+
+    }
+
+    ~cube()
+
+    {
+
+        cout << "\nDestructing " << side << endl;
+
+    }
+
+};
+
+
+
+int main()
+
+{
+
+    cube c1(2.34);
+
+    cube c2;
+
+    cout << "\nThe side of the cube is: " << c1.side << endl;
+
+    cout << "\nThe volume of the first cube is : " << c1.volume() << endl;
+
+    cout << "\nEnter the length of the second cube : ";
+
+    cin >> c2.side;
+
+    cout << "\nThe volume of second cube is : " << c2.volume() << endl;
+
+    return(0);
+
 }
