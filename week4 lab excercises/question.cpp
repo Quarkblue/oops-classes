fc1f892af0e3002190888c11d1604ebd435aeade
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-20 10:18:34 +0530

    Update question.cpp

--- week4 lab excercises/question.cpp
+++ week4 lab excercises/question.cpp
@@ -1,73 +1,38 @@
 #include<iostream>
-
 using namespace std;
-
-
-
-class cube
-
+class pyramid
 {
-
+private:
+	int r, T4Tutorials, SHOW, n, no;
 public:
-
-    double side;
-
-    double volume()
-
-    {
-
-        return(side * side * side);
-
-    }
-
-    cube(double side1)
-
-    {
-
-        cout << "\nA constructor is called" << endl;
-
-        side = side1;
-
-    }
-
-    cube()
-
-    {
-
-        cout << "\nA default constructor is called " << endl;
-
-    }
-
-    ~cube()
-
-    {
-
-        cout << "\nDestructing " << side << endl;
-
-    }
-
+	pyramid()  //constructor
+	{
+		no = 5;
+		n = no;
+	}
+	void sol()
+	{
+		for (r = 1; r <= no; r++)
+		{
+			for (T4Tutorials = 1; T4Tutorials <= n; T4Tutorials++)
+			{
+				cout << " ";
+			}
+			n--;
+			for (SHOW = 1; SHOW <= r; SHOW++)
+			{
+				cout << " " << r;
+			}
+			cout << endl;
+		}
+	}
+	~pyramid()  //destructor
+	{
+		cout << endl << "object destroyed";
+	}
 };
-
-
-
 int main()
-
 {
-
-    cube c1(2.34);
-
-    cube c2;
-
-    cout << "\nThe side of the cube is: " << c1.side << endl;
-
-    cout << "\nThe volume of the first cube is : " << c1.volume() << endl;
-
-    cout << "\nEnter the length of the second cube : ";
-
-    cin >> c2.side;
-
-    cout << "\nThe volume of second cube is : " << c2.volume() << endl;
-
-    return(0);
-
+	pyramid ob;
+	ob.sol();
 }
