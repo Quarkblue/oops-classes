af9bae2e353622276b299c49e1b910a5b756c7f6
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-11 14:14:36 +0530

    10

--- lab week 6/question.cpp
+++ lab week 6/question.cpp
@@ -1,42 +1,22 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
-class func {
+class region {
 public:
-	void pal(int);
+	int volumes;
+	float volume(int a) {
+		return a * a * a;
+	}
+	float volume(float radius, float height) {
+		return 3.14 * radius * radius * height;
+	}
+	float volume(double radius) {
+		return (3.14 * 4 * radius * radius * radius) / 3;
+	}
 };
-void func::pal(int n) {
-	int n1, sum, a;
-	n1 = n;
-	sum = 0;
-	do {
-		sum = sum * 10;
-		a = n % 10;
-		sum = sum + a;
-		n = n / 10;
-	} while (n > 0);
-	if (sum == n1) {
-		cout << "\n It is palindrome";
-	}
-	else {
-		cout << "\n It is not a palindrome";
-	}
-}
 int main() {
-	int k, r, v;
-	func p;
-	do {
-	cout << "Palidrome" << endl;
-		cin >> r;
-		switch (r) {
-		case 1:
-			cout << "Enter the Number";
-			cin >> k;
-			p.pal(k);
-			break;
-		}
-			cout << ”Do you want to continue ? (1 / 0)” << endl;
-			cin >> v;
-		} while (v == 1);
-		return 0;
-	}
+	region x;
+	cout << x.volume(3) << endl;
+	cout << x.volume(3.0, 2.0) <<endl;
+	cout << x.volume(4.0);
+	return 0;
+}
