4f37e5bd8f33f3a6cdb59fa1e02ec983628e980d
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-11 13:48:31 +0530

    8

--- lab Week 5/question.cpp
+++ lab Week 5/question.cpp
@@ -1,32 +1,29 @@
 #include<iostream>
 using namespace std;
-float vol(int, int);
-float vol(float);
-int vol(int);
+class AddAmount
+{
+private:
+	int amount = 50;
+public:
+	int x;
+	AddAmount()
+	{
+		amount + x;
+	}
+	AddAmount(int a)
+	{
+		amount = a + amount;
+	}
+	void displayAmount()
+	{
+		cout << "\n" << amount;
+	}
+};
 int main()
 {
-	int r, h, a;
-	float r1;
-	cout << "Enter radius and height of a cylinder:";
-	cin >> r >> h;
-	cout << "Enter side of cube:";
-	cin >> a;
-	cout << "Enter radius of sphere: ";
-	cin >> r1;
-	cout << "Volume of cylinder is " << vol(r, h);
-	cout << "\nVolume of cube is " << vol(a);
-	cout << "\nVolume of sphere is " << vol(r1);
+	AddAmount a;
+	AddAmount b(40);
+	a.displayAmount();
+	b.displayAmount();
 	return 0;
-}
-float vol(int r, int h)
-{
-	return(3.14 * r * r * h);
-}
-float vol(float r1)
-{
-	return((4 * 3.14 * r1 * r1 * r1) / 3);
-}
-int vol(int a)
-{
-	return(a * a * a);
 }
