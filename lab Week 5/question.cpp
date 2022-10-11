1e15d1c3ef4fa26578606871d051903a7e787460
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-11 13:50:48 +0530

    10

--- lab Week 5/question.cpp
+++ lab Week 5/question.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
+#include <string>
 using namespace std;
-class degree {
-public:
-	degree()
-	{
-		cout << "I got a degree." << endl;
-	}
-};
-class undergraduate :public degree {
-public:
-	undergraduate()
-	{
-		cout << "I am an Undergraduate" << endl;
-	}
-};
-class postgraduate :public degree {
-public:
-	postgraduate()
-	{
-		cout << "I am a Postgraduate";
-	}
-};
-int main()
+class Bank
 {
-	undergraduate u;
-	postgraduate p;
+public:
+	int balance;
+	int getBalance()
+	{
+		return balance;
+	}
+	void setBalance(int i) {
+		balance = i;
+	}
+};
+class BankA : public Bank
+{
+public:
+	int getBalance(int balance)
+	{
+		return balance;
+	}
+};
+class BankB : public Bank
+{
+public:
+	int getBalance(int balance)
+	{
+		return balance;
+	}
+
+};
+class BankC : public Bank
+{
+public:
+	int getBalance(int balance)
+	{
+		return balance;
+	}
+
+};
+int main() {
+	Bank A;
+	A.balance = 1000;
+	cout << "$" << A.getBalance() << endl;
+	A.setBalance(1000);
+
+	Bank B;
+	B.balance = 1500;
+	cout << "$" << B.getBalance() << endl;
+	B.setBalance(1500);
+
+	Bank C;
+	C.balance = 2000;
+	cout << "$" << C.getBalance() << endl;
+	C.setBalance(2000);
 }
