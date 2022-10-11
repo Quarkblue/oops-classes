3cdd2d10eb68d4c41ef383e8c3fb9c260536b40a
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-10-11 14:09:51 +0530

    9

--- lab week 6/question.cpp
+++ lab week 6/question.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
-class shape {
+class func {
 public:
-	int area() {};
-	int cal_area(int length, int breadth) {
-		return length * breadth;
-	}
-	int cal_area(int side) {
-		return side * side;
-	}
+	void pal(int);
 };
-int main() {
-	shape fo{};
-	cout << fo.cal_area(3, 4) << endl;
-	cout << fo.cal_area(5);
-	return 0;
+void func::pal(int n) {
+	int n1, sum, a;
+	n1 = n;
+	sum = 0;
+	do {
+		sum = sum * 10;
+		a = n % 10;
+		sum = sum + a;
+		n = n / 10;
+	} while (n > 0);
+	if (sum == n1) {
+		cout << "\n It is palindrome";
+	}
+	else {
+		cout << "\n It is not a palindrome";
+	}
 }
+int main() {
+	int k, r, v;
+	func p;
+	do {
+	cout << "Palidrome" << endl;
+		cin >> r;
+		switch (r) {
+		case 1:
+			cout << "Enter the Number";
+			cin >> k;
+			p.pal(k);
+			break;
+		}
+			cout << ”Do you want to continue ? (1 / 0)” << endl;
+			cin >> v;
+		} while (v == 1);
+		return 0;
+	}
